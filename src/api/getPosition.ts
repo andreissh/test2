ed4b26c46c03fcd333cce3809edd17bce1883218
andreissh <shkudov.and@gmail.com>
2025-02-13 13:36:44 +0300

    fix location request to locate city instead of a country

--- src/api/getPosition.ts
+++ src/api/getPosition.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const getCityByIP = async () => {
   try {
-    const response = await axios.get("https://get.geojs.io/v1/ip/country.json");
-    return response.data.name;
+    const response = await axios.get("https://get.geojs.io/v1/ip/geo.json");
+    return response.data.city;
   } catch (err) {
     console.error("Ошибка при получении города по IP:", err);
     return null;
